66ce6efffdbd662e955bdea9c3441d9b26403d1e
João Carvalho <Carvalhoestudante1@gmail.com>
2025-01-31 07:27:52 -0300

    First Commit

---
+++ src/main/java/com/example/PlannerStudies/PlannerStudiesApplication.java
@@ -0,0 +1,13 @@
+package com.example.PlannerStudies;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlannerStudiesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlannerStudiesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/PlannerStudies/PlannerStudiesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.PlannerStudies;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlannerStudiesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
